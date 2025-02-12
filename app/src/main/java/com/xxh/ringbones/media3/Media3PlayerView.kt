7b2f1f097d4a2f7c211b1145d3e79eb68bdc9a36
xixiaohui <yamazake@yeah.net>
2025-02-12 22:23:25 +0800

    add Media3 Player

--- app/src/main/java/com/xxh/ringbones/media3/Media3PlayerView.kt
+++ app/src/main/java/com/xxh/ringbones/media3/Media3PlayerView.kt
@@ -1,7 +1,14 @@
 package com.xxh.ringbones.media3
 
+import android.graphics.Color
+import android.graphics.drawable.Drawable
+import android.widget.FrameLayout
+import android.widget.ImageView
+import androidx.annotation.OptIn
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
@@ -9,14 +16,21 @@ import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.media3.common.Player
+import androidx.media3.common.VideoSize
+import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.ui.AspectRatioFrameLayout
 import androidx.media3.ui.PlayerView
+import com.xxh.ringbones.R
 
 
 @Composable
@@ -44,24 +58,42 @@ fun Media3PlayerView(
         modifier = modifier
     ) {
         Media3AndroidView(player)
-        PlayerControls(player)
+//        PlayerControls(player)
     }
 }
 
 
+@OptIn(UnstableApi::class)
 @Composable
 fun Media3AndroidView(player: ExoPlayer?) {
-    AndroidView(
+
+    Box(
         modifier = Modifier
-            .size(width = 300.dp, height = 300.dp)
-            .background(Color.White),
-        factory = { context ->
-            PlayerView(context).apply {
-                this.player = player
-            }
-        },
-        update = { playerView ->
-            playerView.player = player
-        }
-    )
+            .fillMaxSize()
+
+    ){
+
+        AndroidView(
+            factory = { context ->
+                val playView = PlayerView(context).apply {
+                    this.player = player
+                    setBackgroundColor(Color.GREEN)
+                    showController()
+
+                    videoSurfaceView?.setBackgroundColor(Color.TRANSPARENT)
+
+                }
+                playView
+            },
+            update = { playerView ->
+                playerView.player = player
+            },
+            modifier = Modifier.fillMaxSize()
+
+        )
+    }
+
+
+
+
 }
