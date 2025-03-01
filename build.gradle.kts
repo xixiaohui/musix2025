82bbff1e0e7c484ce46415b0f69c6f828db66a4e
Frank <yamazake@yeah.net>
2025-03-01 10:09:45 +0800

    change kotlin 2.1.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
@@ -7,4 +9,6 @@ plugins {
     id("com.google.gms.google-services") version "4.4.2" apply false
 
     id("com.google.devtools.ksp") version "2.1.10-1.0.30" apply false
+
+
 }
