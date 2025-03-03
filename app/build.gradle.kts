21d72f6eed75d810fae0bb830c60d14c56d5e7f2
xixiaohui <yamazake@yeah.net>
2025-03-03 23:13:59 +0800

    query ringtones by typename

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         minSdk = 24
         targetSdk = 35
         versionCode = 17
-        versionName = "2.0.0"
+        versionName = "2.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
